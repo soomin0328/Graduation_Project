53cac2a617faf5e963d82014c4d22fa49020fb6b
soomin0328 <tnalsdhksrms@naver.com>
2018-08-14 13:41:29 +0900

    Commit

--- app/src/main/java/com/neurosky/algo_sdk_sample/EEG.java
+++ app/src/main/java/com/neurosky/algo_sdk_sample/EEG.java
@@ -9,8 +9,6 @@ import android.content.pm.ActivityInfo;
 import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -36,7 +34,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
