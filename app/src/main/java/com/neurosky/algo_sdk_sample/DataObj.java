d42a9cb5377290d768e2b78a57f3709f2cd581e3
soomin0328 <tnalsdhksrms@naver.com>
2018-09-12 23:33:56 +0900

    Commit

---
+++ app/src/main/java/com/neurosky/algo_sdk_sample/DataObj.java
@@ -0,0 +1,26 @@
+package com.neurosky.algo_sdk_sample;
+public class DataObj {
+    public String time;
+    public String val;
+
+    public DataObj(String time, String val) {
+        this.time = time;
+        this.val = val;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+}
---
+++ app/src/main/java/com/neurosky/algo_sdk_sample/PreferenceManager.java
@@ -0,0 +1,44 @@
+package com.neurosky.algo_sdk_sample;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+
+
+public class PreferenceManager {
+
+    Context mContext;
+
+    //컨텍스트를 가져온다.
+    public PreferenceManager(Context context) {
+        this.mContext = context;
+    }
+
+    // preference를 저장한다.
+    public void savePreference(DataObj obj) {
+        SharedPreferences prefs = android.preference.PreferenceManager.getDefaultSharedPreferences(mContext);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        Gson gson = new Gson();
+        String json = gson.toJson(obj);
+        editor.putString("MyObject", json);
+        editor.commit();
+    }
+
+    // preference를 불러온다.
+    public DataObj getPreference() {
+        SharedPreferences prefs = android.preference.PreferenceManager.getDefaultSharedPreferences(mContext);
+        // 저장되었던 코드를 불러와 다시 오브젝트 데이터로 변환하여 리턴
+        Gson gson = new Gson();
+        String json = prefs.getString("MyObject", "");
+        DataObj obj = gson.fromJson(json, DataObj.class);
+        return obj;
+    }
+
+
+    public void clearPreference() {
+        SharedPreferences prefs = android.preference.PreferenceManager.getDefaultSharedPreferences(mContext);
+        prefs.edit().clear().commit();
+    }
+}
